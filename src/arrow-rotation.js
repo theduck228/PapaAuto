a25a364cd8eb16129bad1ac11390774dae44cce1
theduck <theduck228322@gmail.com>
2023-09-17 18:06:25 +0700

    first commit

---
+++ src/arrow-rotation.js
@@ -0,0 +1,5 @@
+const arrow = document.querySelector(".choices[data-type*=select-one]:after")
+const active = document.querySelector(".choices__list--dropdown.is-active")
+element.addEventListener('click', function() {
+    arrow.classList.add('rotated');
+});
---
+++ src/choices.js
@@ -0,0 +1,9 @@
+let element = document.querySelector('.js-choice');
+let choices = new Choices(element, {
+    searchEnabled: false
+});
+
+let element1 = document.querySelector('.js-choice1');
+let choices1 = new Choices(element1, {
+    searchEnabled: false
+});
